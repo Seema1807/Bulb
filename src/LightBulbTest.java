003451da9316a307af55627a25f18cc5b83b3fc9
seema naik <naikseema205@gmail.com>
2017-05-10 12:18:21 -0700

    wattage=200

--- src/LightBulbTest.java
+++ src/LightBulbTest.java
@@ -9,13 +9,13 @@ public class LightBulbTest {
 	LightBulb lightBulb;
 	@Before
 	public void setUp() throws Exception {
-		lightBulb=new LightBulb(100);
+		lightBulb=new LightBulb(200);
 	}
 
 	@Test
 	public void testWattage() {
 		//fail("Not yet implemented");
-		assertEquals(100,lightBulb.getBulbWattage());
+		assertEquals(200,lightBulb.getBulbWattage());
 	}
 
 }
