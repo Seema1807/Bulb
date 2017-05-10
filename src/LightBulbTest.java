2766736a7c171c3799e6b6e9bb6e2b5411e6d20c
seema naik <naikseema205@gmail.com>
2017-05-10 12:16:34 -0700

    wattage=100
    Signed-off-by: seema naik<naikseema205@gmail.com>

--- src/LightBulbTest.java
+++ src/LightBulbTest.java
@@ -9,13 +9,13 @@ public class LightBulbTest {
 	LightBulb lightBulb;
 	@Before
 	public void setUp() throws Exception {
-		lightBulb=new LightBulb(40);
+		lightBulb=new LightBulb(100);
 	}
 
 	@Test
 	public void testWattage() {
 		//fail("Not yet implemented");
-		assertEquals(40,lightBulb.getBulbWattage());
+		assertEquals(100,lightBulb.getBulbWattage());
 	}
 
 }
